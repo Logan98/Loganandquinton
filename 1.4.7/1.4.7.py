25b89f7a90066417b3a33308c9ba78a7f9b1fee9
Logan98 <loganboudreaux2468@gmail.com>
2015-01-09 13:53:28 -0600

    1.4.7

---
+++ 1.4.7/1.4.7.py
@@ -0,0 +1,10 @@
+import PIL
+import matplotlib.pyplot as plt
+import os.path  
+import PIL.ImageDraw            
+
+def add_border(image):
+    
+def add_border_all_images(directory=None)
+        if directory == None:
+        directory = os.getcwd()
